4c8f15ba301069ec186b25c2c589bc13291577bc
meng <missxueo@gmail.com>
2019-03-06 11:14:37 +0800

    demo

---
+++ ConsoleApp3/CustomBackgroundTask.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class CustomBackgroundTask : IHostedService
+    {
+        private ILogger _logger;
+
+        public CustomBackgroundTask(ILogger<CustomBackgroundTask> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"task starting");
+            var task = ExecuteAsync(cancellationToken);
+            if(task.IsCompleted)
+            {
+                return task;
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDefaultHost(args).Build().Run();
+        }
+
+        static IHostBuilder CreateDefaultHost(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureHostConfiguration(builder =>
+                {
+                    builder.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        [HostDefaults.EnvironmentKey] = Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT"),
+                    })
+                    .AddCommandLine(args)
+                    ;
+                })
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    builder.SetBasePath(AppContext.BaseDirectory)
+                        .AddJsonFile("appsettings.json", false, true)
+                        .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", true, true)
+                        ;
+                })
+                .ConfigureServices((ctx, services) =>
+                {
+                    services.AddAutofac();
+                    services.AddLogging(x=> {
+                        x.AddConsole();
+                    });
+                })
+                .ConfigureContainer<ContainerBuilder>(builder => {
+
+                    builder.RegisterType<CustomBackgroundTask>()
+                    .As<IHostedService>()
+                    .InstancePerDependency();
+                })
+                .UseServiceProviderFactory<ContainerBuilder>(new AutofacServiceProviderFactory())
+                .UseConsoleLifetime()
+                ;
+
+        }
+    }
+}
